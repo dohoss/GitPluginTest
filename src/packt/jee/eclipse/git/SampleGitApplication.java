07c120e808ed3c955fa0e18befd1e9b7a913360f
Dohos Sándor <dohoss@gmail.com>
2019-04-06 17:49:18 +0200

    bug#001 1st commit

--- src/packt/jee/eclipse/git/SampleGitApplication.java
+++ src/packt/jee/eclipse/git/SampleGitApplication.java
@@ -5,6 +5,7 @@ public class SampleGitApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st change");
+		System.out.println("Hello from bug#001 branch");
 	}
 
 }
